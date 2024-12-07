797d0b8e876461c123c9e385e679da77af60b6c5
SiddappaA <siddappaa54@gmail.com>
2024-12-07 19:39:53 +0530

    first commit

---
+++ light.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+const light = document.getElementById("light");
+function toggle(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+    
+    
+}
